3c50b54ddf80663816c683822542b35e12a2d744
Komal <102860577+komallllll@users.noreply.github.com>
2026-03-04 21:17:57 +0530

    Create src/index.js as the main entry point for the React application

---
+++ src/index.js
@@ -0,0 +1,7 @@
+// Entry point of the React application
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
